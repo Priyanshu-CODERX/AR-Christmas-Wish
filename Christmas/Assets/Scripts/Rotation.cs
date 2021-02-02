9a7db1500362bc4338c47380785802ddd22c03ec
Priyanshu Bhattacharjee <pbo4oh@gmail.com>
2021-02-02 13:10:04 +0530

    files added

---
+++ Christmas/Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+	public float rotX;
+	public float rotY;
+	public float rotZ;
+    void Update()
+    {
+        transform.Rotate(rotX*Time.deltaTime, rotY*Time.deltaTime, rotZ*Time.deltaTime);
+    }
+}
